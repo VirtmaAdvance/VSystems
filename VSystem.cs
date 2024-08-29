2c00e06f6c58dc9c9ce88ce3389f8522bd6dadb1
Daniel K. Valente <dan.k.valente@gmail.com>
2024-08-29 18:54:20 +0200

    - Added Username and DomainName properties.

--- VSystem.cs
+++ VSystem.cs
@@ -4,6 +4,14 @@ namespace VSystems
 {
 	public class VSystem
 	{
+		/// <summary>
+		/// Gets the currently logged in username.
+		/// </summary>
+		public static string Username => Environment.UserName;
+		/// <summary>
+		/// Gets the current domain name the current user is connected to.
+		/// </summary>
+		public static string DomainName => Environment.UserDomainName;
 
 		/// <summary>
 		/// Locks the current user and returns to the Windows login screen.
