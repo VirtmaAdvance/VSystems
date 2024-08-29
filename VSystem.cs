55aa7a7fe216b02cf3c78d7e30f716c7637544f2
Daniel K. Valente <dan.k.valente@gmail.com>
2024-08-29 15:14:18 +0200

    Add project files.

---
+++ VSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace VSystems
+{
+	public class VSystem
+	{
+
+
+		/// <summary>
+		/// Locks the current user and returns to the Windows login screen.
+		/// </summary>
+		public static void Lock() => LockWorkStation();
+
+		[DllImport("user32.dll")]
+		private static extern bool LockWorkStation();
+
+	}
+}
